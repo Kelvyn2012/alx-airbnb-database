23d537e1c1a682d3846f7259a354dbb5e483fc5b
kelvin udufo <kudufo@gmail.com>
2025-05-25 23:06:33 +0100

    made some chnages adding ORDER BY

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,7 +9,9 @@ SELECT
 FROM
     bookings b
 INNER JOIN
-    users u ON b.user_id = u.id;
+    users u ON b.user_id = u.id
+ORDER BY
+    b.start_date;
 
 -- 2. LEFT JOIN: Get all properties and their reviews (including properties with no reviews)
 SELECT
@@ -21,11 +23,11 @@ SELECT
 FROM
     properties p
 LEFT JOIN
-    reviews r ON p.id = r.property_id;
+    reviews r ON p.id = r.property_id
+ORDER BY
+    p.id;
 
--- 3. FULL OUTER JOIN: Get all users and all bookings, even if no match exists
--- Note: FULL OUTER JOIN is not supported in MySQL. If you're using PostgreSQL, this works.
--- If you're using MySQL, simulate it using UNION.
+-- 3. FULL OUTER JOIN: Get all users and bookings, even if no match exists
 
 -- PostgreSQL version
 SELECT
@@ -38,10 +40,11 @@ SELECT
 FROM
     users u
 FULL OUTER JOIN
-    bookings b ON u.id = b.user_id;
+    bookings b ON u.id = b.user_id
+ORDER BY
+    u.id NULLS LAST, b.id;
 
--- MySQL alternative using UNION
--- SELECT from users LEFT JOIN bookings
+-- MySQL version using UNION
 SELECT
     u.id AS user_id,
     u.first_name,
@@ -56,7 +59,6 @@ LEFT JOIN
 
 UNION
 
--- SELECT from bookings RIGHT JOIN users
 SELECT
     u.id AS user_id,
     u.first_name,
@@ -67,4 +69,6 @@ SELECT
 FROM
     users u
 RIGHT JOIN
-    bookings b ON u.id = b.user_id;
+    bookings b ON u.id = b.user_id
+ORDER BY
+    user_id;
